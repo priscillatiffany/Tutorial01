870dc60de8d149534ad71ab6bab429e53e55b304
Priscilla Tiffany <priscilla.tiffany@ui.ac.id>
2018-09-07 14:25:47 +0700

    Added Dosen.java

---
+++ Dosen.java
@@ -0,0 +1,27 @@
+
+public class Dosen {
+	private String nama, nip;
+
+	public Dosen(String nama, String nip) {
+		super();
+		this.nama = nama;
+		this.nip = nip;
+	}
+
+	public String getNama() {
+		return nama;
+	}
+
+	public void setNama(String nama) {
+		this.nama = nama;
+	}
+
+	public String getNip() {
+		return nip;
+	}
+
+	public void setNip(String nip) {
+		this.nip = nip;
+	}
+
+}
